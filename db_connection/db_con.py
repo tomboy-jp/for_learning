a7235eca7a0b69969a23e14ff3ffc34e9c2e9528
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-13 23:40:18 +0900

    add newtest

---
+++ db_connection/db_con.py
@@ -0,0 +1,23 @@
+import psycopg2
+import csv
+
+# setting_data = '../../data/db_setting.csv'
+setting_data = 'test_settiong.csv'
+dic = {}
+
+with open(setting_data, 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        dic[row[0]] = row[1]
+    f.close
+
+try:
+    con = psycopg2.connect(host=dic['host'], dbname=dic['dbname'], port=dic['port'], user=dic['user'], password=dic['password'])
+    print('Connection succeeded!')
+    cur = con.cursor()
+
+    con.close()
+    cur.close()
+
+except:
+    print('Connection failed...')
