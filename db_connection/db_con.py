3ed0b8e4aa0d3800a8e59f890bee7e2cdeeac02c
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-15 20:38:02 +0900

    unpdate

--- db_connection/db_con.py
+++ db_connection/db_con.py
@@ -2,9 +2,15 @@ import psycopg2
 import csv
 import sys
 
+'''
+test_settingの二列目の値をご使用のDBに合わせてご使用ください。
+PostgreSQLでの使用を想定しているため、他のDBだとport番号も異なるかと思います。
+'''
+
 # setting_data = '../../data/db_setting.csv'
 setting_data = 'test_settiong.csv'
 dic = {}
+result = []
 
 with open(setting_data, 'r') as f:
     reader = csv.reader(f, delimiter=',')
@@ -14,12 +20,20 @@ with open(setting_data, 'r') as f:
 
 try:
     con = psycopg2.connect(host=dic['host'], dbname=dic['dbname'], port=dic['port'], user=dic['user'], password=dic['password'])
-    print('Connection succeeded!')
-    cur = con.cursor()
-
-    con.close()
-    cur.close()
+    print('Connection succeeded!\n')
 
 except:
     print('Connection failed...')
     sys.exit()
+
+sql = "select complete_date, title from nobels where complete_date between '2018/01/01' and '2018/02/28'"
+cur = con.cursor()
+cur.execute(sql)
+
+data = cur.fetchall
+
+con.close()
+cur.close()
+
+for row in data:
+    result.append(row)
