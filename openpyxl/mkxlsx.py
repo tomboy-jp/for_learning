8bca41a3ce58a19faca800798dc4cf13df92497f
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-20 23:43:59 +0900

    update

--- openpyxl/mkxlsx.py
+++ openpyxl/mkxlsx.py
@@ -1,12 +1,18 @@
 import openpyxl as px
 import numpy as np
+import os
+
+l = os.listdir()
+for i in l:
+    if i == "test.xlsx":
+        os.remove("test.xlsx")
 
 wb = px.Workbook()
 ws = wb.active
 
-arr = np.arange(10000).reshape(100,100)
-for y, r in enumerate(arr):
-    for x, val in enumerate(r):
+arr = np.arange(1,10001).reshape(100,100)
+for x, r in enumerate(arr):
+    for y, val in enumerate(r):
         ws.cell(row=x+1, column=y+1).value = val
 
 wb.save("test.xlsx")
