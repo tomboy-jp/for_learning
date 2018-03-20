a16af028c2f069f82626403c2e596b9ad1ff6e4f
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-20 23:36:33 +0900

    update

--- openpyxl/mkxlsx.py
+++ openpyxl/mkxlsx.py
@@ -5,8 +5,8 @@ wb = px.Workbook()
 ws = wb.active
 
 arr = np.arange(10000).reshape(100,100)
-for x, r in enumerate(arr):
-    for y, val in enumerate(r):
+for y, r in enumerate(arr):
+    for x, val in enumerate(r):
         ws.cell(row=x+1, column=y+1).value = val
 
 wb.save("test.xlsx")
