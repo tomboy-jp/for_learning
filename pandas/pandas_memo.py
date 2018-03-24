7eb7c0653e01cef0675ea127c08672b1d20921b3
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-24 14:21:28 +0900

    add pandas dir

---
+++ pandas/pandas_memo.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# numpyから作る場合
+df = pd.DataFrame(np.random.rand(10, 5))
+
+# colmun指定
+df.columns = ["A", "B", "C", "D", "E"]
+
+# "A"が0.5以上の行だけを抽出
+df = df[(df["A"] > 0.5)]
+
+# indexの連番振り直し
+df.reset_index(drop=True)
+
+# dfオブジェクトのforループの廻し方
+for index, row in df.iterrows():
+    print(index, row["B"])
+
+# pandasでグラフを描画
+df.plot(y=["A","B","C","D","E"], colormap='cool', marker='.', markersize=10, title='DataFrameTest', figsize=(10,5), alpha=0.5)
+plt.pause(3)
