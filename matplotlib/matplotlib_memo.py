0c5d454a647303b80cc4d36cf4603d771a7ba628
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-26 23:54:18 +0900

    add matplotlib

---
+++ matplotlib/matplotlib_memo.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+data = np.random.rand(5)
+num = [x for x in range(len(data))]
+
+now = datetime.datetime.now()
+date_label = []
+
+for i in range(len(data)):
+    date_label.append((now - datetime.timedelta(days=i)).strftime("%Y/%m/%d"))
+
+date_label.reverse()
+
+plt.plot(num, data)
+plt.xticks(num, date_label)
+
+plt.pause(5)
