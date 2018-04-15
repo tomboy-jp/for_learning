4d26ccf780a83d491eca1906aba55fdc1953dfec
tomboy <tomboy-jp@users.noreply.github.com>
2018-04-15 23:17:36 +0900

    updarte

--- sklearn/knn_test.py
+++ sklearn/knn_test.py
@@ -1,24 +1,30 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.datasets import load_iris
+from sklearn.datasets import make_classification
+import matplotlib.pyplot as plt
 
-iris_dataset = load_iris()
+# サンプルデータを作成。
+X, y = make_classification(n_samples=1250, n_features=4, n_informative=2, n_redundant=2, random_state=0)
+# よしなに分割。
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    iris_dataset['data'], iris_dataset['target'], random_state=0)
-
+# for文を回す準備。
 k_list = range(1, 11)
-accuracy = []
+train_accuracy = []
+test_accuracy = []
 
+# k近傍法の参照とするデータの数を変化させ、1個から10個までのスコアを抽出。
 for k in k_list:
     model = KNeighborsClassifier(n_neighbors=k)
     model.fit(X_train, y_train)
-    accuracy.append(model.score(X_test, y_test))
+    train_accuracy.append(model.score(X_train, y_train))
+    test_accuracy.append(model.score(X_test, y_test))
 
-plt.plot(k_list, accuracy)
+# トレーニングデータとテストデータの違いをグラフに描画
+plt.plot(k_list, train_accuracy, label="train")
+plt.plot(k_list, test_accuracy, label="test")
 plt.xlabel("n_neighbor")
 plt.ylabel("accuracy")
 plt.title("KNN Test by Iris Data")
+plt.legend()
 plt.show()
--- sklearn/natural_language_vecs.py
+++ sklearn/natural_language_vecs.py
@@ -1,23 +1,20 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+# コサイン類似度を計算する関数
 def cos_sim(v1, v2):
-    similarity = np.dot(v1,v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
+    # np.set_printoptions(precision=3)
+    similarity = np.dot(v1, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
     return similarity
 
-np.set_printoptions(precision=3)
+# テスト用文字列
 words = np.array(["しんじゅく しんじゅく", "しんじゅく じゅくじょ", "じゅくじょ はらじゅく"])
-
-words
-
-vectorizer = TfidfVectorizer(use_idf=True, token_pattern="(?u)\\b\\w+\\b")
-vecs = vectorizer.fit_transform(words)
-vecs = (vecs.toarray())
-vecs
+# 特徴抽出機を設置
+vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u"(?u)\\b\\w+\\b")
+# テスト用文字列をベクトル化
+vecs = vectorizer.fit_transform(words).toarray()
 
 for i1, v1 in enumerate(vecs):
     for i2, v2 in enumerate(vecs):
-        print(
-        "「" + words[i1] + "」 と 「" + words[i2] + "」 のコサイン類似度は" +
-        "%1.3F" % cos_sim(vecs[i1], vecs[i2]) + "です。"
-        )
+        # print(v1, v2)
+        print("「{}」と「{}」のコサイン類似度は{:.3f}です。".format(words[i1], words[i2], cos_sim(vecs[i1], vecs[i2])))
