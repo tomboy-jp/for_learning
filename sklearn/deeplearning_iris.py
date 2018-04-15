b5c3e0a4aa0efe022d010e89c85ce5d13c59109f
tomboy <tomboy-jp@users.noreply.github.com>
2018-04-15 20:42:30 +0900

    update

--- sklearn/deeplearning_iris.py
+++ sklearn/deeplearning_iris.py
@@ -9,10 +9,8 @@ plt.style.use('ggplot')
 
 # irisデータの読み込み。
 data = load_iris()
-
 # 特徴とラベルを格納。
 X, y = data['data'], data['target']
-
 # 標準化のコード。精度が悪化したため、今回は未使用。
 # X = (X - X.mean(axis=0)) / X.std(axis=0)
 
@@ -31,7 +29,7 @@ alpha = [0.001, 0.01, 0.1, 1]
 # 画像サイズの指定と分割。
 fig, axes = plt.subplots(2, 4, figsize=(20, 8))
 
-# 2通りの活性化関数 * 4通りのalpha値) = 8通りの結果を描画して出力。
+# (2通りの活性化関数 * 4通りのalpha値) = 8通りの結果を描画して出力。
 for axx, ac in zip(axes, activation_type):
     for ax, al in zip(axx, alpha):
         model = MLPClassifier(solver="lbfgs", random_state=0, hidden_layer_sizes=[10], activation=ac, alpha=al)
