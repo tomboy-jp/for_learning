a07547ec930ee8737b98b6444c8c60e8d5b81c21
tomboy <tomboy-jp@users.noreply.github.com>
2018-04-17 23:01:05 +0900

    update

---
+++ sklearn/NoTeacher_plot_digits.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+digits = load_digits()
+#
+# fig, axes = plt.subplots(2, 5, figsize=(10, 5),
+#                          subplot_kw={'xticks':(), 'yticks': ()})
+# for ax, img in zip(axes.ravel(), digits.images):
+#     ax.imshow(img)
+#
+# plt.pause(3)
+#
+#
+pca = PCA(n_components=2)
+pca.fit(digits.data)
+
+digits_pca = pca.transform(digits.data)
+colors = ["red", "blue", "green", "yellow", "cyan",
+          "black", "white", "gray", "orange", "magenta"]
+plt.figure(figsize=(10, 10))
+plt.xlim(digits_pca[:, 0].min(), digits_pca[:, 0].max())
+plt.ylim(digits_pca[:, 1].min(), digits_pca[:, 1].max())
+for i in range(len(digits.data)):
+    plt.text(digits_pca[i, 0], digits_pca[i, 1], str(digits.target[i]),
+             color = colors[digits.target[i]],
+             fontdict={'weight': 'bold', 'size': 9})
+plt.xlabel("First principal component")
+plt.ylabel("Second principal component")
+
+plt.pause(10)
+
+
+tsne = TSNE(random_state=42)
+digits_tsne = tsne.fit_transform(digits.data)
+
+plt.figure(figsize=(10, 10))
+plt.xlim(digits_tsne[:, 0].min(), digits_tsne[:, 0].max() + 1)
+plt.ylim(digits_tsne[:, 1].min(), digits_tsne[:, 1].max() + 1)
+
+for i in range(len(digits.data)):
+    plt.text(digits_tsne[i, 0], digits_tsne[i, 1], str(digits.target[i]),
+             color = colors[digits.target[i]],
+             fontdict={'weight': 'bold', 'size': 9})
+
+plt.xlabel("t-SNE feature 0")
+plt.xlabel("t-SNE feature 1")
+
+plt.pause(10)
