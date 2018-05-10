89ffeed040ddefa05a61854f08c2eb9879b280a5
tomboy <tomboy-jp@users.noreply.github.com>
2018-05-10 23:48:47 +0900

    update

--- OneLiner/listoneliner.py
+++ OneLiner/listoneliner.py
@@ -2,17 +2,13 @@
 l = [[1,2,3,4,5],[6,7,8,9,10]]
 
 # 二重ループ
-la = [x for y in l for x in y]
-print(la)
+print([x for y in l for x in y])
 
 # 二重ループ + if
-lb = [x for y in l for x in y if x%2==0]
-print(lb)
+print([x for y in l for x in y if x%2==0])
 
 # 二重ループ + if + else
-lc = [x if x%2==0 else 0 for y in l for x in y]
-print(lc)
+print([x if x%2==0 else 0 for y in l for x in y])
 
 # FizzBuzzワンライナ-
-fizzbuzz = ["FizzBuzz" if x%15==0 else "Fizz" if x%3==0 else "Buzz" if x%5==0 else x for x in range(1,100)]
-print(fizzbuzz)
+print(["FizzBuzz" if x%15==0 else "Fizz" if x%3==0 else "Buzz" if x%5==0 else x for x in range(1,100)])
