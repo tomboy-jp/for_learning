90b9f2f57c7e4434937c6d3a54981718f21369da
tomboy <tomboy-jp@users.noreply.github.com>
2018-05-12 23:56:11 +0900

    update

--- OneLiner/listoneliner.py
+++ OneLiner/listoneliner.py
@@ -12,3 +12,6 @@ print([x if x%2==0 else 0 for y in l for x in y])
 
 # FizzBuzzワンライナ-
 print(["FizzBuzz" if x%15==0 else "Fizz" if x%3==0 else "Buzz" if x%5==0 else x for x in range(1,100)])
+
+# execワンライナー
+exec("def test(n):\n\tfor x in range(n):\n\t\tprint(x)"); t = test(100);
