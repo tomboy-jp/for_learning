4147d09001ae5b366a8de1d8ca140efde69318b5
tomboy <tomboy-jp@users.noreply.github.com>
2018-08-19 23:02:37 +0900

    update

--- node/nodetest.js
+++ node/nodetest.js
@@ -1,7 +1,8 @@
 console.log('this is Server Side JavaScript\n');
+
 const list = ['this', 'is', 'Server', 'Side', 'JavaScript'];
 
 for(var i=0,l=list.length;i<l;i++){
-  console.log(list[i])
+  console.log(list[i]);
 }
 
